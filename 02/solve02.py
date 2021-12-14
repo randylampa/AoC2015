f3e40df8c5b00908222b3abd8cf9ddc661452346
René Panák <rene.panak@seznam.cz>
2021-12-15 00:10:56 +0100

    solve-02-2

--- 02/solve02.py
+++ 02/solve02.py
@@ -30,39 +30,48 @@ def solve_part_1():
 	packages = read_file_into_packages(fn)
 	#~ print(packages)
 
-	paper_area = 0
+	total_paper_area = 0
 	for package in packages:
 		#~ print(package)
 		areas = get_package_unique_areas(package)
 		#~ print(areas)
-		total_area = sum(list(map(lambda x:2*x,areas))) + min(areas)
-		#~ print(total_area)
-		paper_area+=total_area
+		paper_area = sum(list(map(lambda x:2*x,areas))) + min(areas)
+		#~ print(paper_area)
+		total_paper_area+=paper_area
 
-	answer = paper_area
+	answer = total_paper_area
 
 	"""<<< Do something here"""
 	utils.print_answer(1, demo, answer)
 
 def solve_part_2():
-	demo = 1
+	demo = 0
 
 	fn = utils.get_input_file(demo, ISSUE);
 	print(fn)
 	"""Do something here >>>"""
+	packages = read_file_into_packages(fn)
+	#~ print(packages)
 
-	print('Part 2 not solved yet')
+	total_ribbon_length = 0
+	for package in packages:
+		#~ print(package)
+		wrap = 2*(sum(package)-max(package))
+		bow = package[0] * package[1] * package[2]
+		ribbon_length = wrap + bow
+		print(ribbon_length)
+		total_ribbon_length+=ribbon_length
 	
-	answer = None
+	answer = total_ribbon_length
 
 	"""<<< Do something here"""
 	utils.print_answer(2, demo, answer)
 
 def main(args):
 	
-	solve_part_1()
+	#~ solve_part_1()
 	
-	#~ solve_part_2()
+	solve_part_2()
 	
 	return 0
 
