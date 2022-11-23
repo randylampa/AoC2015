0d34b14818af9aba79fc62335f0418d1b5204c60
René Panák <rene.panak@seznam.cz>
2022-11-24 00:13:47 +0100

    solve-03-2

--- 03/solve03.py
+++ 03/solve03.py
@@ -40,6 +40,7 @@ def solve_part_1():
 
 	fn = utils.get_input_file(demo, ISSUE, True);
 	print(fn)
+	"""Do something here >>>"""
 
 	f = open(fn, 'r')
 	chars = f.read()
@@ -53,25 +54,69 @@ def solve_part_1():
 	"""<<< Do something here"""
 	utils.print_answer(1, demo, answer)
 
+def make_list_of_coords_agents(direction_instr, unique:bool=False, agents:int=1):
+
+	point = (0,0)
+	coords = [point]
+	
+	agent_points = []
+	for i in range(agents):
+		agent_points.append(point)
+	counter = 0
+	
+	for ins in direction_instr:
+		point = agent_points[counter] # load point of current agent
+		if ins=="^":
+			point = (point[0], point[1]+1)
+		elif ins=="v":
+			point = (point[0], point[1]-1)
+		elif ins==">":
+			point = (point[0]+1, point[1])
+		elif ins=="<":
+			point = (point[0]-1, point[1])
+		else:
+			#print("wrong input char")
+			continue
+			
+		agent_points[counter] = point # store point of current agent
+
+		if not unique or unique and not (point in coords):
+			#print("add: ins=",ins,"point=",point)
+			coords.append(point)
+#		else:
+#			print("dupl: ins=",ins,"point=",point)
+
+		counter+=1
+		if counter>=agents:
+			#print("counter wrap")
+			counter = 0
+
+	return coords
+
 def solve_part_2():
-	demo = 1
+	demo = 0
 
 	fn = utils.get_input_file(demo, ISSUE, True);
 	print(fn)
 	"""Do something here >>>"""
 
-	print('Part 2 not solved yet')
-	
-	answer = None
+	f = open(fn, 'r')
+	chars = f.read()
+	#print(chars)
+
+	coords = make_list_of_coords_agents(chars, True, 2)
+	#print(coords)
+
+	answer = len(coords)
 
 	"""<<< Do something here"""
 	utils.print_answer(2, demo, answer)
 
 def main(args):
 	
-	solve_part_1()
+#	solve_part_1()
 	
-#	solve_part_2()
+	solve_part_2()
 	
 	return 0
 
