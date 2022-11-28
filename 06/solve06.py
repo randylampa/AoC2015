ccd570d59adafb5cadc5c2a7ce27e5c78bbf0623
René Panák <rene.panak@seznam.cz>
2022-11-28 20:07:00 +0100

    solve-06-2 right

--- 06/solve06.py
+++ 06/solve06.py
@@ -12,35 +12,32 @@ ISSUE=6
 gridsize = 1000
 #~ gridsize = 10
 
-def parse_instructions(lines:list) -> list:
-	dinstrs = []
-	for line in lines:
-		il = line.split()
+def parse_instruction(line:str) -> dict:
+	il = line.strip().split()
 
-		dinstr = {
-			'cmd':None,
-			'begin':None,
-			'end':None
-		}
+	dinstr = {
+		'cmd':None,
+		'begin':None,
+		'end':None
+	}
 
-		if il[0]=='turn':
-			# ~ turn on/off
-			dinstr['cmd'] = il[1]
-			dinstr['begin'] = tuple(map(int,il[2].split(',')))
-			dinstr['end'] = tuple(map(int,il[4].split(',')))
-			pass
-		else:
-			# ~ toggle
-			dinstr['cmd'] = il[0]
-			dinstr['begin'] = tuple(map(int,il[1].split(',')))
-			dinstr['end'] = tuple(map(int,il[3].split(',')))
-			pass
-		# TODO: test gridsize limits
-		dinstrs.append(dinstr)
-	return dinstrs
+	if il[0]=='turn':
+		# turn on/off
+		dinstr['cmd'] = il[1]
+		dinstr['begin'] = tuple(map(int,il[2].split(',')))
+		dinstr['end'] = tuple(map(int,il[4].split(',')))
+		pass
+	else:
+		# toggle
+		dinstr['cmd'] = il[0]
+		dinstr['begin'] = tuple(map(int,il[1].split(',')))
+		dinstr['end'] = tuple(map(int,il[3].split(',')))
+		pass
+	# TODO: test gridsize limits
+	return dinstr
 
 def init_grid(gridsize:int, default_value:int = 0):
-	#~ return [[default_value] * gridsize] * gridsize # TOTO NEJEDE! (dělá duplikáty objektů)
+	#~ return [[default_value] * gridsize] * gridsize # TOTO NEJEDE! (dělá duplikáty objektů - listů)
 	return [[default_value for x in range(gridsize)] for x in range(gridsize)]
 
 def print_grid(light_grid:list):
@@ -63,20 +60,6 @@ def grid_apply(light_grid:list, begin:tuple, end:tuple, cmd:str):
 	#~ print_grid(light_grid)
 	#~ print(light_grid)
 
-def grid_apply2(light_grid:list, begin:tuple, end:tuple, cmd:str):
-	#~ print("grid_apply", (cmd, begin, end))
-	for y in range(begin[1], end[1] + 1):
-		for x in range(begin[0], end[0] + 1):
-			val = light_grid[y][x]
-			if cmd == 'toggle':
-				val2 = val+2
-			else:
-				val2 = val+1 if cmd=='on' else val-1
-			#~ print((x,y,cmd,val,val2))
-			light_grid[y][x] = val2
-	#~ print_grid(light_grid)
-	#~ print(light_grid)
-
 def grid_count_on(light_grid:list) -> int:
 	cnt = 0
 	for line in light_grid:
@@ -91,24 +74,44 @@ def solve_part_1(demo:bool) -> str:
 	"""Do something here >>>"""
 
 	light_grid = init_grid(gridsize)
-	#~ print_grid(light_grid)
+	# ~ print_grid(light_grid)
 
-	# ~ parse
-	dinstrs = parse_instructions(utils.read_file_into_list(fn))
-	#~ print(dinstrs)
+	# parse
+	dinstrs = utils.read_file_into_list(fn, parse_instruction)
+	# ~ print(dinstrs)
 
-	# ~ apply
+	# apply
 	for dinstr in dinstrs:
 		grid_apply(light_grid, dinstr['begin'], dinstr['end'], dinstr['cmd'])
 
-	#~ print_grid(light_grid)
+	# ~ print_grid(light_grid)
 
-	# ~ count on
+	# count on
 	answer = grid_count_on(light_grid)
 
 	"""<<< Do something here"""
 	utils.print_answer(1, demo, answer)
 	return answer
+
+def grid_apply2(light_grid:list, begin:tuple, end:tuple, cmd:str):
+	# ~ print("grid_apply", (cmd, begin, end))
+	for y in range(begin[1], end[1] + 1):
+		for x in range(begin[0], end[0] + 1):
+			val = light_grid[y][x]
+			if cmd == 'toggle':
+				val2 = val+2
+			elif cmd == 'off':
+				val2 = (val-1) if val>0 else 0
+			elif cmd == 'on':
+				val2 = val+1
+			else:
+				# nochange
+				print('UNKNOWN COMMAND', cmd)
+				val2 = val
+			# ~ print((x,y,cmd,val,val2))
+			light_grid[y][x] = val2
+	# ~ print_grid(light_grid)
+	# ~ print(light_grid)
 
 def solve_part_2(demo:bool) -> str:
 
@@ -119,17 +122,17 @@ def solve_part_2(demo:bool) -> str:
 	"""Do something here >>>"""
 
 	light_grid = init_grid(gridsize)
-	#~ print_grid(light_grid)
+	# ~ print_grid(light_grid)
 
-	# ~ parse
-	dinstrs = parse_instructions(utils.read_file_into_list(fn))
-	#~ print(dinstrs)
+	# parse
+	dinstrs = utils.read_file_into_list(fn, parse_instruction)
+	# ~ print(dinstrs)
 
-	# ~ apply
+	# apply
 	for dinstr in dinstrs:
 		grid_apply2(light_grid, dinstr['begin'], dinstr['end'], dinstr['cmd'])
 
-	# ~ count on
+	# count on
 	answer = grid_count_on(light_grid)
 
 	"""<<< Do something here"""
@@ -138,10 +141,13 @@ def solve_part_2(demo:bool) -> str:
 
 def main(args):
 
-	#~ solve_part_1(0)
+	solve_part_1(0)
+	# right answer: 569999
 
-	#~ 17325717 is too low!
+	# 17325717 is too low!
+	# beware of going lower than zero (to negatives!)
 	solve_part_2(0)
+	# right answer: 17836115
 
 	return 0
 
