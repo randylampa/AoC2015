38908bc5bf280b0d7ca9b2d8c867af856984a100
René Panák <panak@advertising-media.cz>
2022-11-28 13:49:53 +0100

    solve-06-1

--- 06/solve06.py
+++ 06/solve06.py
@@ -10,36 +10,12 @@ import utils
 ISSUE=6
 
 gridsize = 1000
-light_grid = [[0] * gridsize] * gridsize
-# ~ print(light_grid)
+#~ gridsize = 10
 
-def grid_apply(begin:tuple, end:tuple, cmd:str):
-	for x in range(begin[0], end[0]):
-		for y in range(begin[1], end[1]):
-			val = light_grid[x][y]
-			if cmd == 'toggle':
-				light_grid[x][y] = 0 if val==1 else 1
-			else:
-				light_grid[x][y] = 1 if cmd=='on' else 0
-	pass
-
-def grid_count_on() -> int:
-	cnt = 0
-	for line in light_grid:
-		cnt+=sum(line)
-	return cnt
-
-def solve_part_1(demo:bool) -> str:
-
-	fn = utils.get_input_file(demo, ISSUE, True)
-	print(fn)
-	"""Do something here >>>"""
-
-	# ~ parse
-	instrs0 = utils.read_file_into_list(fn)
-	instrs = []
-	for instr in instrs0:
-		il = instr.split()
+def parse_instructions(lines:list) -> list:
+	dinstrs = []
+	for line in lines:
+		il = line.split()
 
 		dinstr = {
 			'cmd':None,
@@ -59,16 +35,62 @@ def solve_part_1(demo:bool) -> str:
 			dinstr['begin'] = tuple(map(int,il[1].split(',')))
 			dinstr['end'] = tuple(map(int,il[3].split(',')))
 			pass
+		# TODO: test gridsize limits
+		dinstrs.append(dinstr)
+	return dinstrs
 
-		instrs.append(dinstr)
-	print(instrs)
+def init_grid(gridsize:int, default_value:int = 0):
+	#~ return [[default_value] * gridsize] * gridsize # TOTO NEJEDE! (dělá duplikáty objektů)
+	return [[default_value for x in range(gridsize)] for x in range(gridsize)]
+
+def print_grid(light_grid:list):
+	#~ print("light_grid")
+	#~ print(light_grid)
+	for line in light_grid:
+		print(line)
+
+def grid_apply(light_grid:list, begin:tuple, end:tuple, cmd:str):
+	#~ print("grid_apply", (cmd, begin, end))
+	for y in range(begin[1], end[1] + 1):
+		for x in range(begin[0], end[0] + 1):
+			val = light_grid[y][x]
+			if cmd == 'toggle':
+				val2 = 0 if val==1 else 1
+			else:
+				val2 = 1 if cmd=='on' else 0
+			#~ print((x,y,cmd,val,val2))
+			light_grid[y][x] = val2
+	#~ print_grid(light_grid)
+	#~ print(light_grid)
+
+def grid_count_on(light_grid:list) -> int:
+	cnt = 0
+	for line in light_grid:
+		cnt+=sum(line)
+	return cnt
+
+def solve_part_1(demo:bool) -> str:
+
+	fn = utils.get_input_file(demo, ISSUE, True)
+	#~ fn+='a';
+	print(fn)
+	"""Do something here >>>"""
+
+	light_grid = init_grid(gridsize)
+	#~ print_grid(light_grid)
+
+	# ~ parse
+	dinstrs = parse_instructions(utils.read_file_into_list(fn))
+	#~ print(dinstrs)
 
 	# ~ apply
-	for inst in instrs:
-		grid_apply(dinstr['begin'], dinstr['end'], dinstr['cmd'])
+	for dinstr in dinstrs:
+		grid_apply(light_grid, dinstr['begin'], dinstr['end'], dinstr['cmd'])
+
+	#~ print_grid(light_grid)
 
 	# ~ count on
-	answer = grid_count_on()
+	answer = grid_count_on(light_grid)
 
 	"""<<< Do something here"""
 	utils.print_answer(1, demo, answer)
@@ -80,9 +102,19 @@ def solve_part_2(demo:bool) -> str:
 	print(fn)
 	"""Do something here >>>"""
 
-	print('Part 2 not solved yet')
+	light_grid = init_grid(gridsize)
+	#~ print_grid(light_grid)
 
-	answer = None
+	# ~ parse
+	dinstrs = parse_instructions(utils.read_file_into_list(fn))
+	#~ print(dinstrs)
+
+	# ~ apply
+	for dinstr in dinstrs:
+		grid_apply(light_grid, dinstr['begin'], dinstr['end'], dinstr['cmd'])
+
+	# ~ count on
+	answer = grid_count_on(light_grid)
 
 	"""<<< Do something here"""
 	utils.print_answer(2, demo, answer)
@@ -90,7 +122,7 @@ def solve_part_2(demo:bool) -> str:
 
 def main(args):
 
-	solve_part_1(1)
+	solve_part_1(0)
 
 	# ~ solve_part_2(1)
 
