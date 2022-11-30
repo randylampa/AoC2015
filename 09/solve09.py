9cdac15c48d656640fe354e627f73e45f7c3d1f0
René Panák <rene.panak@seznam.cz>
2022-11-30 23:59:01 +0100

    s-09 - city_map with names... unnecessary

--- 09/solve09.py
+++ 09/solve09.py
@@ -10,9 +10,13 @@ import utils
 
 ISSUE=9
 
-def parse_line(line:str) -> tuple:
-	mm = re.search("(\w+) to (\w+) = (\d+)", line)
-	return (mm.group(1), mm.group(2), int(mm.group(3)))
+def parse_line(line:str) -> dict:
+	mm = re.search("(?P<A>\w+) to (?P<B>\w+) = (?P<dist>\d+)", line)
+	dm = mm.groupdict()
+	dm['dist'] = int(dm['dist'])
+	# ~ print(dm)
+	return dm
+	# ~ return (mm.group(1), mm.group(2), int(mm.group(3)))
 
 def solve_part_1(demo:bool) -> str:
 
@@ -21,7 +25,32 @@ def solve_part_1(demo:bool) -> str:
 	"""Do something here >>>"""
 
 	inputs = utils.read_file_into_list(fn, parse_line)
-	print(inputs)
+	# ~ print(inputs)
+
+	city_map = {}
+	cm_def = {
+		'name': None,
+		'dists': {},
+	}
+	for inp in inputs:
+		nA = inp['A']
+		nB = inp['B']
+		if nA not in city_map:
+			city_map[nA] = {
+				'name': nA,
+				'dists': {},
+			}
+		if nB not in city_map:
+			city_map[nB] = {
+				'name': nB,
+				'dists': {},
+			}
+		dA = city_map[nA]
+		dB = city_map[nB]
+		dA['dists'][nB] = inp['dist']
+		dB['dists'][nA] = inp['dist']
+		# ~ print('ooj', dA, dB)
+	print(city_map)
 
 	answer = None
 
@@ -47,7 +76,7 @@ def main(args):
 
 	solve_part_1(1)
 
-	solve_part_2(1)
+	# ~ solve_part_2(1)
 
 	return 0
 
