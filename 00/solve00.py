afce216bbee53c7bbb6d5bc14e3102798dd59b74
René Panák <panak@advertising-media.cz>
2022-12-01 10:05:18 +0100

    solve00 comments

--- 00/solve00.py
+++ 00/solve00.py
@@ -9,6 +9,9 @@ import utils
 
 ISSUE=0
 
+"""
+	SOLVE PART 1
+"""
 def solve_part_1(demo:bool) -> str:
 
 	fn = utils.get_input_file(demo, ISSUE, True)
@@ -16,13 +19,16 @@ def solve_part_1(demo:bool) -> str:
 	"""Do something here >>>"""
 
 	print('Part 1 not solved yet')
-	
+
 	answer = None
 
 	"""<<< Do something here"""
 	utils.print_answer(1, demo, answer)
 	return answer
 
+"""
+	SOLVE PART 2
+"""
 def solve_part_2(demo:bool) -> str:
 
 	fn = utils.get_input_file(demo, ISSUE, True)
@@ -30,19 +36,22 @@ def solve_part_2(demo:bool) -> str:
 	"""Do something here >>>"""
 
 	print('Part 2 not solved yet')
-	
+
 	answer = None
 
 	"""<<< Do something here"""
 	utils.print_answer(2, demo, answer)
 	return answer
 
+"""
+	MAIN
+"""
 def main(args):
-	
+
 	solve_part_1(1)
-	
+
 	solve_part_2(1)
-	
+
 	return 0
 
 if __name__ == '__main__':
