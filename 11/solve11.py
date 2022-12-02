dab2bae7b54c611caffbe7323923410ec88d91a7
René Panák <rene.panak@seznam.cz>
2022-12-02 01:06:03 +0100

    update HTMLs

---
+++ 11/solve11.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# Aoc 2015 - 11
+
+import sys
+sys.path.append('..')
+import utils
+
+ISSUE=11
+
+INPUT="vzbxkghb"
+
+"""
+	SOLVE PART 1
+"""
+def solve_part_1(demo:bool) -> str:
+
+	fn = utils.get_input_file(demo, ISSUE, True)
+	print(fn)
+	"""Do something here >>>"""
+
+	print('Part 1 not solved yet')
+
+	answer = None
+
+	"""<<< Do something here"""
+	utils.print_answer(1, demo, answer)
+	return answer
+
+"""
+	SOLVE PART 2
+"""
+def solve_part_2(demo:bool) -> str:
+
+	fn = utils.get_input_file(demo, ISSUE, True)
+	print(fn)
+	"""Do something here >>>"""
+
+	print('Part 2 not solved yet')
+
+	answer = None
+
+	"""<<< Do something here"""
+	utils.print_answer(2, demo, answer)
+	return answer
+
+"""
+	MAIN
+"""
+def main(args):
+
+	solve_part_1(1)
+
+	solve_part_2(1)
+
+	return 0
+
+if __name__ == '__main__':
+	sys.exit(main(sys.argv))
