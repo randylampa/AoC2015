4b2af60e7e25b44bdaede166581eb7161d22de9f
René Panák <rene.panak@seznam.cz>
2024-11-28 22:57:15 +0100

    utils - new read_file_into_dict

--- utils.py
+++ utils.py
@@ -5,6 +5,7 @@
 #
 
 import sys
+import re
 
 def get_input_file(demo, issue:int = None, sensible:bool = False) -> str:
 	s = str(issue).rjust(2,'0') if issue is not None else ''
@@ -16,22 +17,26 @@ def get_output_file(demo, issue:int = None, sensible:bool = False) -> str:
 	f = 'output{}' if not demo else 'output{}-demo' if not sensible else 'output-demo{}'
 	return f.format(s)
 
-def read_file_into_list(name = 'input', mapfnc = lambda x:x.strip()) -> list:
+def read_file_into_lines(name:str = 'input', mode:str = 'r') -> list:
 	"""
-	Reads all lines into list and map mapfnc on each.
+	eads all lines into list
 	"""
 	f = open(name, 'r')
 	lines = f.readlines()
 	f.close()
-	return [*map(mapfnc, lines)]
+	return lines
+
+def read_file_into_list(name = 'input', mapfnc = lambda x:x.strip()) -> list:
+	"""
+	Reads all lines into list and map mapfnc on each.
+	"""
+	return [*map(mapfnc, read_file_into_lines(name))]
 
 def read_file_into_list_bin(name = 'input', mapfnc = None) -> list:
 	"""
 	Reads all lines into list and map mapfnc on each.
 	"""
-	f = open(name, 'rb')
-	lines = f.readlines()
-	f.close()
+	lines = read_file_into_lines(name, 'rb')
 	if mapfnc is not None:
 		return [*map(mapfnc, lines)]
 	else:
@@ -45,9 +50,21 @@ def read_file_into_list_of_ints(name = 'input') -> list:
 
 def read_file_into_lists_of_ints(name = 'input') -> list:
 	"""
-	Read all lines into lis of lists (sep=,)
+	Read all lines into list of lists (sep=,)
 	"""
 	return read_file_into_list(name, lambda x: [*map(int, x.strip().split(','))])
+
+def read_file_into_dict(name:str = 'input', kvpattrn:str='(?P<k>.*):\s*(?P<v>.*)', vmap = int) -> dict:
+	"""
+	Reads all lines into dict and maps vmap on each value
+	"""
+	outdict = {}
+	for line in read_file_into_lines(name):
+		mm = re.search(kvpattrn, line)
+		k = mm.groupdict()['k']
+		v = mm.groupdict()['v']
+		outdict[k] = vmap(v)
+	return outdict
 
 def dump_list_of(xlist:list):
 	for x in xlist: print(x)
